840e75806c1162f61b3983ddef5e53dc2d76574a
Thao Le <thaolt@songphi.com>
2014-11-19 15:42:37 +0700

    basic functions and icons

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,71 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFileDialog>
+#include <QDir>
+#include <QPixmap>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QDir::setCurrent(QDir::homePath());
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_btnTaskAdd_clicked()
+{
+    ui->tblTasks->insertRow( ui->tblTasks->rowCount() );
+}
+
+void MainWindow::on_btnTaskRemove_clicked()
+{
+}
+
+void MainWindow::on_btnEnvRemove_clicked()
+{
+
+}
+
+void MainWindow::on_btnEnvAdd_clicked()
+{
+    ui->tblEnvVars->insertRow( ui->tblEnvVars->rowCount() );
+}
+
+void MainWindow::on_btnSelectSplash_clicked()
+{
+    QFileDialog *fileDlg = new QFileDialog(this, tr(""),0,"Images (*.png *.gif *.jpg *.bmp)");
+    fileDlg->setAcceptMode(QFileDialog::AcceptOpen);
+    if (fileDlg->exec()) {
+        QString fileName = fileDlg->selectedFiles().first();
+        QPixmap splash(fileName);
+        ui->lblPreviewSplash->setPixmap( splash );
+        ui->tbxSplashFile->setText( fileName );
+    }
+}
+
+void MainWindow::on_action_Quit_triggered()
+{
+    QApplication::quit();
+}
+
+void MainWindow::on_actionAbout_Qt_triggered()
+{
+    QApplication::aboutQt();
+}
+
+void MainWindow::on_toolButton_6_clicked()
+{
+    QFileDialog *fileDlg = new QFileDialog(this, tr(""),0,"Icons (*.ico)");
+    fileDlg->setAcceptMode(QFileDialog::AcceptOpen);
+    if (fileDlg->exec()) {
+        QString fileName = fileDlg->selectedFiles().first();
+        QPixmap icon(fileName);
+        ui->lblPreviewIcon->setPixmap( icon );
+        ui->tbxIconFile->setText( fileName );
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,23 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_btnTaskAdd_clicked();
+
+    void on_btnTaskRemove_clicked();
+
+    void on_btnEnvRemove_clicked();
+
+    void on_btnEnvAdd_clicked();
+
+    void on_btnSelectSplash_clicked();
+
+    void on_action_Quit_triggered();
+
+    void on_actionAbout_Qt_triggered();
+
+    void on_toolButton_6_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
